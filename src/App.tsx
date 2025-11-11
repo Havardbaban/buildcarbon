25787f111151421e9ceb3670a743c00c723715ac
Håvard Baban <havardbaban@gmail.com>
2025-11-11 10:18:30 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,21 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Demo from "./pages/Demo";
 import Benchmark from "./pages/Benchmark";
-import EnvestoReport from "./EnvestoReport"; // use as a simple /report page
+import Invoices from "./pages/Invoices"; // NEW
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/demo" element={<Demo />} />
         <Route path="/benchmark" element={<Benchmark />} />
-        <Route
-          path="/report"
-          element={
-            <div className="mx-auto max-w-6xl p-6">
-              <EnvestoReport
-                inputs={{
-                  energySpend: 100000,
-                  reductionPct: 15,
-                  grant: 10000,
-                  loanAmount: 500000,
-                  rateCurrent: 4.0,
-                  rateGreen: 3.5,
-                  capex: 200000,
-                  taxRate: 22,
-                  consultantFees: 5000,
-                }}
-                companyName="Acme AS"
-                projectTitle="Energy Efficiency Upgrade"
-              />
-            </div>
-          }
-        />
+        <Route path="/invoices" element={<Invoices />} /> {/* NEW */}
       </Routes>
     </BrowserRouter>
   );
 }
+
